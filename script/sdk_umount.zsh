13924480b4a2a564125693af00a457c423f69b84
chn <897331845@qq.com>
2020-04-25 20:21:26 +0800

    fix sdk umount

--- script/sdk_umount.zsh
+++ script/sdk_umount.zsh
@@ -1,10 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
 
-cd $(dirname $0)
+cd $(dirname $0:A)/sdk
 sdks=$(ls | grep -v -E "(upper|lower|work)")
 
-for sdk in $sdks
+for sdk in $(echo $sdks)
 do
     sudo umount $sdk
     sudo umount $sdk.lower
 done
+
+cd ..
+rm -rf sdk
