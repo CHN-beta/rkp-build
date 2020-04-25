8d2dcce2a2858799b30f505f54a9df03a4308e05
chn <897331845@qq.com>
2020-04-25 20:17:20 +0800

    fix sdk_mount.zsh

--- script/sdk_mount.zsh
+++ script/sdk_mount.zsh
@@ -1,17 +1,16 @@
 #!/bin/zsh
 
-sdk_dir=$1:A
 cd $(dirname $0:A)
 mkdir sdk
 cd sdk
 
-for img in $(ls $sdk_dir)
+for img in $(ls ../../sdk)
 do
     sdk=$(echo $img | sed 's/\(.*\)\.img/\1/g')
     mkdir $sdk
     mkdir $sdk.upper
     mkdir $sdk.lower
     mkdir $sdk.work
-    sudo mount -o ro $sdk_dir/$sdk.img $sdk.lower
+    sudo mount -o ro ../../sdk/$sdk.img $sdk.lower
     sudo mount -t overlay overlay -o upperdir=$sdk.upper,lowerdir=$sdk.lower,workdir=$sdk.work $sdk
 done
