7cd748a07fa1046726764c1580a0eab6811a103d
chn <897331845@qq.com>
2020-04-25 21:00:36 +0800

    fix build.zsh

--- script/build.zsh
+++ script/build.zsh
@@ -1,40 +1,26 @@
 #/bin/zsh
 
-build() {
-    # args: version target subtarget sdk url dir
-
-    version=$1
-    target=$2
-    subtarget=$3
-    sdk=$4
-    url=$5
-    dir=$6
-    echo "version=$version target=$target subtarget=$subtarget sdk=$sdk url=$url dir=$dir"
-
-    # download and prepare
-    cd "$dir"
-    echo "here we go" >> build.log
-    echo $@ >> build.log
-    curl -O --retry 99 $url
-    tar -xvf $sdk.tar.xz
-    cd $sdk
-    git clone https://github.com/CHN-beta/xmurp-ua.git package/xmurp-ua
-    git clone https://github.com/CHN-beta/rkp-ipid.git package/rkp-ipid
-
-    # compile
-    make defconfig >> ../build.log 2>&1 
-    make package/xmurp-ua/compile V=sc >> ../build.log 2>&1
-    make package/rkp-ipid/compile V=sc >> ../build.log 2>&1
-
-    # copy and clean
-    mkdir -p ../bin/$version/$target/$subtarget
-    cp bin/targets/*/*/packages/* ../bin/$version/$target/$subtarget
-    cd "$dir"
-    rm -rf $sdk*
-}
-
-dir=$(pwd)
-cat list.txt | while read line
+cd $(dirname $0:A)
+build_dir=$(pwd)
+cd sdk
+for version in $(ls | grep -v -E "(upper|lower|work)")
 do
-    build $(echo $line) "$dir"
+    cd $build_dir/sdk/$version
+    for target in $(ls)
+    do
+        cd $build_dir/sdk/$version/$target
+        for subtarget in $(ls)
+        do
+            cd $build_dir/sdk/$version/$target/$subtarget/*/
+            cp -rf $build_dir/package .
+            make defconfig
+            for package in $(ls $build_dir/package)
+	    do
+                make package/$package/compile V=sc -j8
+            done
+            mkdir -p $build_dir/bin/$version/$target/$subtarget
+            cp bin/targets/*/*/packages/* $build_dir/bin/$version/$target/$subtarget/
+        done
+    done
 done
+
